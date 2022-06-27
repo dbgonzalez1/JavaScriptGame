4c5a7d50967c59daacfe93f0b7c2087327e4c6e2
dbgonzalez1 <dbgonzalez1@espe.edu.ec>
2022-06-27 12:14:09 -0500

    Creación de carpetasstatic y templates

---
+++ index.py
@@ -0,0 +1,17 @@
+#importamos la libreria Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#---------------------------------------
+#Ruta de pagina principal
+#---------------------------------------
+
+@app.route('/')
+def index():
+    return render_template('game.html')
+
+#---------------------------------------
+#iniciamos la aplicacion
+if __name__ == '__main__':
+    app.run(debug=True)
